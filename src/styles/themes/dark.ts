2a5258072adbb1714bc86b7fe603ae46ea7bc934
matheusthiesen <matheusreis314@gmail.com>
2021-07-27 00:36:45 -0300

    Atulizando temas

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -4,7 +4,6 @@ export default {
   colors: {
     primery: "#ff0",
     secundary: "#f00",
-
     background: "#333",
     text: "#fff",
   },
@@ -12,5 +11,12 @@ export default {
     small: "10px",
     normal: "12px",
     large: "14px",
+  },
+  fonts: {
+    title700: "Rajdhani_700Bold",
+    title500: "Rajdhani_500Medium",
+    text400: "Inter_400Regular",
+    text500: "Inter_500Medium",
+    text700: "Inter_700Bold",
   },
 };
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,3 +1,5 @@
+import { dynamicFontSizing } from "../../utils/dynamicFontSizing";
+
 export default {
   title: "light",
 
@@ -5,12 +7,19 @@ export default {
     primery: "#ff0",
     secundary: "#f00",
 
-    background: "#f5f5f5",
+    background: "#f1f1f1",
     text: "#333",
   },
   sizes: {
-    small: "10px",
-    normal: "12px",
-    large: "14px",
+    small: `${dynamicFontSizing(12)}px`,
+    normal: `${dynamicFontSizing(14)}px`,
+    large: `${dynamicFontSizing(16)}px`,
+  },
+  fonts: {
+    title700: "Rajdhani_700Bold",
+    title500: "Rajdhani_500Medium",
+    text400: "Inter_400Regular",
+    text500: "Inter_500Medium",
+    text700: "Inter_700Bold",
   },
 };
